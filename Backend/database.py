112c295f980fddc7a0f969230cb5dea419eb58db
Shubham Sonal Bharucha <shubham.bharucha@Yash.com>
2026-09-10 14:47:49 +0530

    Updated datase.py. Changed host from on-prem 10.9.16.101 server to new AWS-DB server 10.0.54.52

--- Backend/database.py
+++ Backend/database.py
@@ -3,7 +3,7 @@ from datetime import date, datetime
 
 def get_connection():
     return psycopg2.connect(
-        host="10.9.16.101",
+        host="10.0.54.52",
         port=5432,
         database="central_licensing",
         user="postgres",
